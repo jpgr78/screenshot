73359cd1c79eb903feb8faa5e185a4204c0c473e
MacMini18 <mcm-mac-18@MacMini18s-Mac-mini.local>
2022-03-08 10:45:35 -0800

    first

---
+++ test/pageobjects/dynamic_loading.page.js
@@ -0,0 +1,27 @@
+const Page = require('./page');
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class DynamicLoadingPage extends Page {
+    /**
+     * define selectors using getter methods
+     */
+     get startButton() {
+        return $('#start button'); //uses Element with certain text selector
+   }
+  /**
+   * overwrite specific options to adapt it to page object
+   */
+   async startClick() {
+      await this.startButton.click();
+  }
+  get htest(){
+      return $('#finish h4');
+  }
+  open(path) {
+      return super.open(path);
+  }
+}
+
+module.exports = new DynamicLoadingPage();
---
+++ test/specs/example.e2e.js
@@ -0,0 +1,23 @@
+const LoginPage = require('../pageobjects/login.page');
+const SecurePage = require('../pageobjects/secure.page');
+const DynamicLoadingPage = require('../pageobjects/dynamic_loading.page');
+
+
+describe('My Dynamic Loading Page', () => {
+    it('should verify that start button exists', async () => {
+        await DynamicLoadingPage.open('dynamic_loading/1');
+
+        await expect(DynamicLoadingPage.startButton).toBeExisting();
+        await browser.saveScreenshot('./screenshot.png');
+        
+    });
+     it('click', async () => {
+        await DynamicLoadingPage.open('dynamic_loading/1');
+        await DynamicLoadingPage.startClick()
+        await DynamicLoadingPage.startButton.waitForDisplayed({ timeout: 5000, reverse : true });
+        await expect(DynamicLoadingPage.htest).toBeDisplayed();
+        await browser.saveScreenshot('./screenshot2.png');
+    });
+});
+
+
